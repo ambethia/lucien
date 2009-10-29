137fd3bddb6f6f81bbed79cc648a1095b60de8b7
Jason L Perry <jasper@ambethia.com>
2009-10-29 09:44:15 -0400

    Setup Haml/Sass & jQuery

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def render_flash
+    if flash[:notice]
+      render :partial => "layouts/flash", :locals => { :message => flash[:notice] }
+    end
+  end
 end
