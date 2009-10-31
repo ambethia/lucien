766e55e0c27a2db21a66565582b28e1d0e856716
Jason L Perry <jasper@ambethia.com>
2009-10-31 04:52:31 -0400

    Fix dependencies

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,10 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'chriseppstein-compass', :lib => 'compass', :source => 'http://gems.github.com/'
-  config.gem 'haml', :lib => 'haml', :version => '>=2.2.0'
   config.time_zone = 'UTC'
+
   config.gem 'haml'
+  config.gem 'compass'
   config.gem 'authlogic'
   config.gem 'authlogic-oid', :lib => 'authlogic_openid'
   config.gem 'paperclip'
