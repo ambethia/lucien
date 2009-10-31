04baf869d4c5bf2e2cd9cb1b71e1e86592cd9678
Jason L Perry <jasper@ambethia.com>
2009-10-31 03:14:46 -0400

    Compass for stylesheets

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,8 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem 'chriseppstein-compass', :lib => 'compass', :source => 'http://gems.github.com/'
+  config.gem 'haml', :lib => 'haml', :version => '>=2.2.0'
   config.time_zone = 'UTC'
   config.gem 'haml'
   config.gem 'authlogic'
