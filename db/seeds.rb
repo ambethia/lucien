91e6522a246fe51b8b5329ed66cda8c710000c77
Jason L Perry <jasper@ambethia.com>
2009-11-10 23:18:15 -0500

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+Role.find_or_create_by_name(:name => 'member', :title => 'Member')
+Role.find_or_create_by_name(:name => 'admin', :title => 'Administrator')
+
+user = User.new
+user.signup!({:name => 'Jason L Perry', :email => "jasper@ambethia.com"}, false)
+user.activate!({:openid_identifier => 'http://ambethia.com'}, false)
+user.roles << Role.find_by_name('admin')
+
