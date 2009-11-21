2497976e50c2cb0c7eba61ee37d7b011c639876b
Jason L Perry <jasper@ambethia.com>
2009-11-21 14:38:49 -0500

    Some cleanup

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,24 +1,8 @@
 authorization do
-  role :guest, :title => "Guest" do
-    description "The default role for anonymous user"
-    # Don't remove this or you can't signup
-    has_permission_on :users, :to => :create
-  end
-
   role :member, :title => "Member" do
     description "The default role for authenticated user"
     #show/update account, invite friends
     has_permission_on :users, :to => [:show, :update, :create]
-  end
-
-  role :author, :title => "Author" do
-    description "The default role for author"
-    has_permission_on :users, :to => [:show, :update]
-  end
-
-  role :manager, :title => "Manager" do
-    description "The default role for manager"
-    has_permission_on :users, :to => [:show, :update]
   end
 
   role :admin, :title => "Administrator" do
